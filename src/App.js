6013af0d601e3773155e51c4c0ddb5a172b450c0
Brandon Jurado <brandonjurado13@yahoo.com>
2017-09-30 18:25:31 -0500

    Initial commit w/search functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchField from './SearchField';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      userInput: '',
+      searchResponse: []
+    }
+  }
+
+  handleButtonClick =() => {
+    fetch('https://www.googleapis.com/books/v1/volumes?q=${this.state.userInput}')
+    .then(response => response.json())
+    .then(response => this.setState({ searchResponse: response.items }))
+  }
+
+  handleUserInput = (event) => {
+    console.log(event.target.value);
+    this.setState({ userInput: event.target.value })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <SearchField
+      handleButtonClick = {this.handleButtonClick}
+      handleUserInput = {this.handleUserInput}/>
       </div>
     );
   }
