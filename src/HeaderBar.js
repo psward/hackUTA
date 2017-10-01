e3cc96b715be62c153ec1544159641b54616d3b2
preston <prestonsward@gmail.com>
2017-09-30 19:13:33 -0500

    good luck

--- src/HeaderBar.js
+++ src/HeaderBar.js
@@ -1,5 +1,10 @@
 import React from  'react';
 import {Navbar, Nav, NavItem} from 'react-bootstrap';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom'
 
 // Uses boostrap components to build a header bar...
 const HeaderBar = (props) => {
@@ -8,6 +13,21 @@ const HeaderBar = (props) => {
       <Navbar.Header>
         <Navbar.Brand>
           <a href="#">FoodBunk</a>
+            <Router>
+                <div>
+                    <ul>
+                        <lu><lunk to="/">Home</lunk></lu>
+                        <lu><lunk to="/items">Build your own recipe</lunk></lu>
+                        <lu><lunk to="/recipes">Find a recipe</lunk></lu>
+                    </ul>
+
+                    <hr/>
+
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/items" component={Items}/>
+                    <Route path="/recipes" component={Recipes}/>
+                </div>
+            </Router>
         </Navbar.Brand>
         <Navbar.Toggle />
       </Navbar.Header>
@@ -20,5 +40,23 @@ const HeaderBar = (props) => {
     </Navbar>
   )
 }
+
+const Home = () => (
+    <div>
+        <h2>Home</h2>
+    </div>
+)
+
+const Items = () => (
+    <div>
+        <h2>Items</h2>
+    </div>
+)
+
+const Recipes = ({ match }) => (
+    <div>
+        <h2>Recipes</h2>
+    </div>
+)
 
 export default HeaderBar;
