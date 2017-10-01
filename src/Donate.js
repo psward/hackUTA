0a96e5e2577e8bd2797769f0684a52a3d09148ca
preston <prestonsward@gmail.com>
2017-10-01 00:08:19 -0500

    i hate links and buttons and stuff

---
+++ src/Donate.js
@@ -0,0 +1,12 @@
+import React from  'react';
+import './Donate.css';
+
+const Donate = () => (
+    <div>
+        <h2>Donate here</h2>
+        <p>testestewt</p>
+
+    </div>
+)
+
+export default Donate;
--- src/HeaderBar.js
+++ src/HeaderBar.js
@@ -1,10 +1,17 @@
 import React from  'react';
 import {Navbar, Nav, NavItem} from 'react-bootstrap';
+import Donate from './Donate.js'
+import { LinkContainer } from 'react-router-bootstrap';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+} from 'react-router-dom';
 
 // Uses boostrap components to build a header bar...
 const HeaderBar = (props) => {
   return (
-    <Navbar inverse>
+      <Navbar inverse>
       <Navbar.Header>
         <Navbar.Brand>
           <a href="#">FoodBunk</a>
@@ -13,12 +20,21 @@ const HeaderBar = (props) => {
       </Navbar.Header>
       <Navbar.Collapse>
         <Nav pullRight>
-          <NavItem eventkey={1} href="#">Donate</NavItem>
+            <LinkContainer to="/donate">
+                <NavItem eventkey={2} href="/donate">Donate</NavItem>
+            </LinkContainer>
+            <Router>
+            <Route path ="/donate" component={Donate}/>
+            </Router>
           <NavItem eventkey={2} href="#">Search</NavItem>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
   )
 }
+
+const DonateScreen = () => (
+    <Donate/>
+)
 
 export default HeaderBar;
