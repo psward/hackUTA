61ef9e8d0ded68209e47713da243317b5454aa72
preston <prestonsward@gmail.com>
2017-10-01 10:08:14 -0500

    name change

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import React from  'react';
 const Home = () => (
     <body className="home">
       <div className="main">
-          <h1>FoodBunk</h1>
+          <h1>Food Scrapper</h1>
           <h2>An easy way to shop for recipes!</h2>
       </div>
     </body>
