2f4dab4ed7b460764779371ea365a08221f16f77
preston <prestonsward@gmail.com>
2017-10-01 10:43:53 -0500

    compost page

--- src/Compost.js
+++ src/Compost.js
@@ -1,10 +1,32 @@
 import React from  'react';
 
 const Home = () => (
-      <div className="main">
+      <body className="compost">
           <h1>How To Compost Food Scraps</h1>
-          <p></p>
-      </div>
+          <p>How quickly compost breaks down depends on four things – moisture, oxygen content, temperature, and a good mix of ingredients. The perfect compost pile is damp without being wet, like a squeezed out sponge. It should also be well aerated, with plenty of the oxygen that aerobic bacteria need. And it should have a mix of different types of materials. If you have just one thing, like grass clippings alone, or leaves alone, then it takes a really long time to break down. But if you have several materials and mix them all together, then they break down much more quickly.
+
+              <p> To achieve optimal conditions, here are the things you should focus on:</p>
+
+              <p>1) Drainage.
+              2) Air flow.
+              3) Insulation.
+              4) Good Mix of Various Ingredients.</p>
+
+              <p>Good compost bins allow liquids to drain easily. Stagnant water can suffocate helpful bacteria, and allowing water to sit practically invites pests such as mosquitoes or raccoons. Many compost bins also offer some cover. A lid that keeps rain and snow out of the compost pile will protect the compost from excess liquid. Even if there’s a drain at the bottom of the bin (such as a spigot or mesh screen), it’s a good idea to avoid putting too much water in in the first place. Water that trickles through the compost can wash away useful nutrients and will also suck away heat. However, if you keep the lid on your compost bin all the time, you’ll want to check and make sure that you’re getting ENOUGH water, because materials don’t break down very well if they are too dry either. The general rule of thumb is that materials in your composter or compost pile should be as moist as a damp, wrung out sponge.</p>
+
+              <p>If you live in an arid area, you’ll want to keep an eye on your bin in the summer months to make sure that it stays damp enough when it sits in the direct sun. If it gets too dry, add water with the hose or uncover the pile or bin when it rains. If it seems too damp, leave the lid off in the sun and let some of the moisture out.</p>
+
+              <p>Many compost bins are designed to maximize air flow and circulation. This is because oxygen keeps away the stinky anaerobic microbes that can otherwise create odors. Keeping your compost well mixed and aerated prevents those anaerobic bacteria from getting established. Oxygen is fuel for the aerobic microbes that break down trash into finished compost – give them plenty of oxygen and they’ll work quickly. There are tumbling compost bins that help aerate their contents, or you may want to use a compost turning tool like a pitchfork or a specialized compost turning tool.</p>
+
+              <p>Temperature is another key to the breakdown of compost. Helpful microbes work best at elevated temperatures, so it’s important to keep their workplace warm. The fastest decomposition occurs between 140 and 160 degrees Fahrenheit, but some breakdown will occur as long as the compost is kept above freezing. Microbes generate their own heat as they work, so keeping the compost bin warm is all about proper insulation. Compost is self insulating – if you pile compost on top of compost, the middle of your compost stack will be the warmest. Position your compost so that the wind doesn’t blow on it and so that it sits in full sunlight. One way to get extra heat is to choose a compost bin with dark colored walls that warm up in the sun. Or if you are using a pile, put a black tarp over it. In hotter areas of the country, you might need to pick a shady spot instead.</p>
+
+              <p>The ingredients of your compost are also important. Microbes that break down compost do well on a mix of different food sources. They need plenty of cellulose-rich, carbon material along with nitrogen rich kitchen scraps. The ideal mix is 75% “brown” material and 25% “green” scraps by volume. Or if you are doing it by weight, it’s about a 50 / 50 mix of nitrogen rich and carbon rich materials. That means for every pound of kitchen scraps, it’s good to have a pound of leaves, for example.</p>
+
+              <p>“Brown” material includes dried grass, leaves, and shredded newspaper. These types of material take longer to break down than “green” vegetable peels and fruit rinds. If your compost is too wet, add more brown material. If you don’t have a lot of green material, you may need to add water to the compost.</p>
+
+              <p>Green and brown doesn’t refer to the actual color of the stuff you are putting in the composter — it’s just shorthand for saying nitrogen rich or carbon rich.</p>
+          </p>
+      </body>
 )
 
 export default Home;
